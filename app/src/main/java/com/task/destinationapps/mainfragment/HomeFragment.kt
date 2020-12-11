29dd732fa7b9b1335a64a50332c5993476a2589c
reihanbay <bung.reihan28@gmail.com>
2020-12-11 11:07:43 +0700

    'upload-task'

--- app/src/main/java/com/task/destinationapps/mainfragment/HomeFragment.kt
+++ app/src/main/java/com/task/destinationapps/mainfragment/HomeFragment.kt
@@ -1,19 +1,13 @@
 package com.task.destinationapps.mainfragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.task.destinationapps.R
 import com.task.destinationapps.adapter.BestTourAdapter
 import com.task.destinationapps.adapter.TopTourAdapter
